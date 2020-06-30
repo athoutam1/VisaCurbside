2b33ab4b51e7152695aeedaf9384f817bfbc3677
Jose Alexis Con <josealexiscon@Michaels-MacBook-Pro.local>
2020-06-29 20:58:59 -0600

    fixed firestore calls

--- api/routes/messageMerchant.js
+++ api/routes/messageMerchant.js
@@ -6,9 +6,11 @@ const admin = require("../services/firebase").admin;
 
 router.get("/", async function (req, res) {
   const { message, storeID, userID } = req.query;
-  console.log(req.query)
+  
+  
   const chatRef = db.collection("chats").doc(`${userID}AND${storeID}`);
-  await chatRef.update({
+  try {
+    await chatRef.update({
       messages: admin.firestore.FieldValue.arrayUnion({
       message: message,
       messenger: "user",
@@ -19,7 +21,22 @@ router.get("/", async function (req, res) {
     //     messenger: "user",
     //   },
     // ],
-  });
+    });
+  } catch (e) {
+    await chatRef.set({
+    //   messages: admin.firestore.FieldValue.arrayUnion({
+    //   message: message,
+    //   messenger: "user",
+    // })
+    messages: [
+      {
+        message,
+        messenger: "user",
+      },
+    ],
+    });
+    console.log(e)
+  }
 
   try {
     var config = {
